f549fb96c386d9cf0ffce997d909ca9955f5f46c
Dustni <nofacer@outlook.com>
2015-08-30 23:43:37 +0800

    随便改点

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -22,26 +22,10 @@
 					<h1 id="logo"><a href="/intro">Alphacker</a></h1>
 					<nav id="nav">
 						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li>
-								<a href="#">Layouts</a>
-								<ul>
-									<li><a href="left-sidebar.html">Left Sidebar</a></li>
-									<li><a href="right-sidebar.html">Right Sidebar</a></li>
-									<li><a href="no-sidebar.html">No Sidebar</a></li>
-									<li>
-										<a href="#">Submenu</a>
-										<ul>
-											<li><a href="#">Option 1</a></li>
-											<li><a href="#">Option 2</a></li>
-											<li><a href="#">Option 3</a></li>
-											<li><a href="#">Option 4</a></li>
-										</ul>
-									</li>
-								</ul>
-							</li>
-							<li><a href="elements.html">Elements</a></li>
-							<li><a href="#" class="button special">Sign Up</a></li>
+							
+							
+							
+							<li><a href="#" class="button special">注册</a></li>
 						</ul>
 					</nav>
 				</header>
@@ -199,7 +183,7 @@
 						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
 					</ul>
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li>&copy; Alphacker All rights reserved.</li><li>Design: <a href="http://html5up.net">Dustni</a></li>
 					</ul>
 				</footer>
 
