03412261b4c723aea306289eed6cf4beab3df29e
Dustni <nofacer@outlook.com>
2015-08-31 09:56:30 +0800

    INTRO页面更改

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -24,8 +24,8 @@
 						<ul>
 							
 							
-							
-							<li><a href="#" class="button special">注册</a></li>
+							<li><a href="index.html">主页</a></li>
+							<li><a href="/auth/register" class="button special ">注册</a></li>
 						</ul>
 					</nav>
 				</header>
@@ -34,9 +34,9 @@
 				<section id="banner">
 					<div class="content">
 						<header>
-							<h2>The future has landed</h2>
-							<p>And there are no hoverboards or flying cars.<br />
-							Just apps. Lots of mother flipping apps.</p>
+							<h2>重新定义教育</h2>
+							
+							<a href="#" class="button  small">免费试用</a>
 						</header>
 						<span class="image"><img src="images/intro/pic01.jpg" alt="" /></span>
 					</div>
